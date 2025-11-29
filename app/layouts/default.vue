ca58c0fc792fed439f2fc9f10d24942c005c816e
chafikdeveloper <chafikdevelopper@gmail.com>
2025-11-29 13:44:57 +0100

    added the about page

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,28 +1,32 @@
 <script setup lang="ts"></script>
 
 <template>
-    <!-- //TODO: uncomment this when you finish -->
-  <UiSplashCursor
-    :SIM_RESOLUTION="128"
-    :DYE_RESOLUTION="1440"
-    :CAPTURE_RESOLUTION="512"
-    :DENSITY_DISSIPATION="3.5"
-    :VELOCITY_DISSIPATION="2"
-    :PRESSURE="0.1"
-    :PRESSURE_ITERATIONS="20"
-    :CURL="3"
-    :SPLAT_RADIUS="0.2"
-    :SPLAT_FORCE="6000"
-    :SHADING="true"
-    :COLOR_UPDATE_SPEED="10"
-    :BACK_COLOR="{ r: 0.5, g: 0, b: 0 }"
-    :TRANSPARENT="true"
-  />
-  <Navbar />
+  <!-- //TODO: uncomment this when you finish -->
+  <div class="min-h-screen flex flex-col">
+    <UiSplashCursor
+      :SIM_RESOLUTION="128"
+      :DYE_RESOLUTION="1440"
+      :CAPTURE_RESOLUTION="512"
+      :DENSITY_DISSIPATION="3.5"
+      :VELOCITY_DISSIPATION="2"
+      :PRESSURE="0.1"
+      :PRESSURE_ITERATIONS="20"
+      :CURL="3"
+      :SPLAT_RADIUS="0.2"
+      :SPLAT_FORCE="6000"
+      :SHADING="true"
+      :COLOR_UPDATE_SPEED="10"
+      :BACK_COLOR="{ r: 0.5, g: 0, b: 0 }"
+      :TRANSPARENT="true"
+    />
+    <Navbar />
 
-  <main class="container mx-auto max-w-7xl">
-    <slot />
-  </main>
+    <main class="container flex-1 grow mx-auto max-w-7xl">
+      <slot />
+    </main>
 
-  <Footer />
+    <div>
+      <Footer />
+    </div>
+  </div>
 </template>
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,13 +1,107 @@
-<script setup lang='ts'>
+<script setup lang="ts">
 definePageMeta({
-    name: 'about'
-})
+  name: "about",
+});
 
 useHead({
-    title: 'Chafik Chegma | About'
-})
+  title: "Chafik Chegma | About",
+});
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold text-center mt-8 text-black dark:text-white">About</h1>
+  <section
+    class="flex flex-col items-center md:items-start space-y-3 px-6 py-3 lg:p-6 mx-auto max-w-7xl mt-8 lg:mt-16"
+  >
+    <div class="md:w-2/3">
+      <h2 class="text-3xl font-bold mb-4">About Me</h2>
+      <p class="text-lg text-black dark:text-white font-medium">
+        Chafik Chegma, Algerian full-stack developer. Graduated in 2024 in
+        "Master's Radio Mobile Network". Start coding since 2022 with interest
+        in web developement transforming problems to solutions. I've worked with
+        many technologies using <span class="framework">Javascript</span>,
+        <span class="framework">Typescript</span> and
+        <span class="framework">php</span>, familiar with popular frameworks out
+        there like: <span class="framework">Vue.js, Nuxt.js, Astro.js</span> for
+        the frontend and
+        <span class="framework">Laravel and Express.js</span> for the backend
+        along with SQL databases:
+        <span class="framework">MySQL, PostgreSQL</span>
+      </p>
+    </div>
+  </section>
+
+  <section
+    class="flex flex-col items-center md:items-start space-y-3 px-6 py-3 lg:p-6 mx-auto max-w-7xl mt-8"
+  >
+    <div class="md:w-2/3">
+      <h2 class="text-3xl font-bold mb-4">My Journey</h2>
+
+      <ol class="relative border-s border-gray-700">
+        <li class="mb-10 ms-4">
+          <div
+            class="absolute w-3 h-3 bg-emerald-600 rounded-full mt-1.5 -start-1.5"
+          ></div>
+          <time
+            class="text-sm font-normal leading-none text-black dark:text-white"
+            >27th, April - 30th, October 2025</time
+          >
+          <h3 class="text-lg font-semibold text-heading my-2">
+            Full-Stack Developer Internship
+          </h3>
+          <p
+            class="mb-4 text-base font-normal text-body text-black dark:text-white"
+          >
+            6 month Internship at TiniTec
+          </p>
+        </li>
+
+        <li class="mb-10 ms-4">
+          <div
+            class="absolute w-3 h-3 bg-emerald-600 rounded-full mt-1.5 -start-1.5"
+          ></div>
+          <time
+            class="text-sm font-normal leading-none text-black dark:text-white"
+            >September 2022 - June 2024</time
+          >
+          <h3 class="text-lg font-semibold text-heading my-2">
+            Master's Radio Mobile Network
+          </h3>
+          <p
+            class="mb-4 text-base font-normal text-body text-black dark:text-white"
+          >
+            Obtained my Master's Degree in Radio Mobile Network from Sciences
+            and Technology University Houari Boumediene
+          </p>
+        </li>
+
+        <li class="mb-10 ms-4">
+          <div
+            class="absolute w-3 h-3 bg-emerald-600 rounded-full mt-1.5 -start-1.5"
+          ></div>
+          <time
+            class="text-sm font-normal leading-none text-black dark:text-white"
+            >October 2019 - July 2022</time
+          >
+          <h3 class="text-lg font-semibold text-heading my-2">
+            Bachelor's Telecommunication
+          </h3>
+          <p
+            class="mb-4 text-base font-normal text-body text-black dark:text-white"
+          >
+            Obtained my BAchelor's Degree in Telecommunication from Sciences and
+            Technology University Houari Boumediene
+          </p>
+        </li>
+      </ol>
+    </div>
+    <div class="mt-8">
+      <button
+        class="px-4 py-2 rounded-md bg-emerald-600 dark:bg-emerald-800 text-emerald-200 dark:text-emerald-400 font-bold cursor-pointer"
+      >
+        <NuxtLink to="/cv-chafik-chegma.pdf" target="_blank"
+          >Download my Resume (CV)</NuxtLink
+        >
+      </button>
+    </div>
+  </section>
 </template>
